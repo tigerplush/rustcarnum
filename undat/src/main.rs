456b8d27dd0eaa28cd6f611190c5d023c0d24aa0
tigerplush <christopher.putz@corgibase.de>
2025-05-09 15:31:33 +0200

    added indicatif to show some kind of feedback to users

--- undat/src/main.rs
+++ undat/src/main.rs
@@ -1,12 +1,20 @@
-use std::{fs::{self, File}, path::Path};
+use std::{fs, path::Path, time::Instant};
 
 use clap::Parser;
+use console::{Emoji, style};
 use dat_entry::{DatEntry, DatEntryType};
 use dat_footer::DatFooter;
+use indicatif::{HumanDuration, ProgressBar};
 use zune_inflate::DeflateDecoder;
 
 mod dat_entry;
 mod dat_footer;
+
+static LOOKING_GLASS: Emoji<'_, '_> = Emoji("🔍  ", "");
+static FOOTER: Emoji<'_, '_> = Emoji("🦶  ", "");
+static SPARKLE: Emoji<'_, '_> = Emoji("✨ ", ":-)");
+static PARSER: Emoji<'_, '_> = Emoji("📋  ", "");
+static EXTRACTING: Emoji<'_, '_> = Emoji("💾  ", "");
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -18,30 +26,58 @@ struct Settings {
 }
 
 fn main() -> std::io::Result<()> {
+    let started = Instant::now();
     let args = Settings::parse();
+    println!(
+        "{} {}Reading file...",
+        style("[1/4]").bold().dim(),
+        LOOKING_GLASS
+    );
     let data: Vec<u8> = fs::read(&args.input_filepath)?;
-    println!("total file size: {}", data.len());
+    println!(
+        "{} {}Extracting footer...",
+        style("[2/4]").bold().dim(),
+        FOOTER
+    );
     let footer_data = &data[data.len() - DatFooter::LENGTH..];
     let footer = DatFooter::try_from(footer_data).unwrap();
-    println!("footer: {:?}", footer);
     let filetable_start = data.len() - footer.dat_entry_start_from_end;
-    println!(
-        "filetable: {:#x?}",
-        &data[filetable_start..filetable_start + 8]
+
+    let entry_count = u32::from_le_bytes(
+        data[filetable_start..filetable_start + 4]
+            .try_into()
+            .unwrap(),
     );
     let mut entry_start = filetable_start + 8;
     let mut entries = Vec::new();
+    println!(
+        "{} {}Parsing file entries...",
+        style("[3/4]").bold().dim(),
+        PARSER
+    );
+    let progress_bar = ProgressBar::new(entry_count as u64);
     while entry_start < data.len() - DatFooter::LENGTH {
         let entry = DatEntry::try_from(&data[entry_start..]).unwrap();
-        println!("entry: {:?}, len: {}", entry, entry.len());
+        // println!("entry: {:?}, len: {}", entry, entry.len());
         entry_start += entry.len();
         entries.push(entry);
+        progress_bar.inc(1);
     }
+    progress_bar.finish_and_clear();
 
-    println!("found {} entries", entries.len());
-
+    println!(
+        "{} {}Extracting files...",
+        style("[4/4]").bold().dim(),
+        EXTRACTING
+    );
+    let progress_bar = ProgressBar::new(entries.len() as u64);
     for entry in entries {
         let path = Path::new(&args.output_filepath).join(&entry.filepath);
+        if let Some(parent) = path.parent() {
+            if !fs::exists(parent)? {
+                fs::create_dir_all(parent)?;
+            }
+        }
         match entry.entry_type {
             DatEntryType::Directory => {
                 if !fs::exists(&path)? {
@@ -49,10 +85,11 @@ fn main() -> std::io::Result<()> {
                 }
             }
             DatEntryType::Compressed => {
-                let mut decoder = DeflateDecoder::new(&data[entry.offset..entry.offset+entry.deflate_size]);
+                let mut decoder =
+                    DeflateDecoder::new(&data[entry.offset..entry.offset + entry.deflate_size]);
                 let decompressed_data = decoder.decode_zlib().unwrap();
                 fs::write(&path, decompressed_data)?;
-            },
+            }
             DatEntryType::Stored => {
                 fs::write(
                     &path,
@@ -60,7 +97,10 @@ fn main() -> std::io::Result<()> {
                 )?;
             }
         }
+        progress_bar.inc(1);
     }
+    progress_bar.finish_and_clear();
 
+    println!("{} Done in {}", SPARKLE, HumanDuration(started.elapsed()));
     Ok(())
 }
